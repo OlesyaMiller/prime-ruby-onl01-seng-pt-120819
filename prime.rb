3a9a20cf869a1be98b8943c1040ba98e5aa67cc4
Olesya Miller <olesyam.miller@gmail.com>
2019-12-12 23:01:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  my_range = (2..integer - 1).to_a 
+
+  # return false if integer == 0 
+  # return true if integer == 1 || integer == 2 
+
+  my_range.any? { |divider|
+    #if integer % divider == 0 
+      return false
+    #end
+    # if integer % divider != 0 && integer > 2 
+    #   return true 
+    # end 
+  }
+end
