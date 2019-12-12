8b2ef358d2375311e2b5564fed60822967f66cb9
Olesya Miller <olesyam.miller@gmail.com>
2019-12-12 23:25:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,9 @@ require 'pry'
 
 def prime?(integer)
   my_range = (2..integer - 1).to_a 
-  if integer == 2 || integer == 3 
+  if integer == 2 || integer == 3 || integer < 0 
     return true
+    
   end
   my_range.any? { |divider|
     if integer % divider == 0 
